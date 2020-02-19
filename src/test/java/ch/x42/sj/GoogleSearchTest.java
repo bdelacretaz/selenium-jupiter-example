309fb41d04f45e69fd7f419f40c6380d31c93fae
Bertrand Delacretaz <bdelacretaz@apache.org>
2020-02-19 17:38:37 +0100

    SauceLabs setup

--- src/test/java/ch/x42/sj/GoogleSearchTest.java
+++ src/test/java/ch/x42/sj/GoogleSearchTest.java
@@ -13,10 +13,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import io.github.bonigarcia.seljup.DockerBrowser;
 import io.github.bonigarcia.seljup.DriverCapabilities;
@@ -29,9 +31,20 @@ import io.github.bonigarcia.seljup.SeleniumExtension;
 @ExtendWith(SeleniumExtension.class)
 public class GoogleSearchTest {
 
+  // Testing with SauceLabs
   @DriverUrl
-  // Start selenium-grid-docker-compose.yml to activate this hub
-  private final String url = "http://localhost:4444/wd/hub";
+  static final String url = "https://ondemand.eu-central-1.saucelabs.com/wd/hub";
+
+  @DriverCapabilities
+  DesiredCapabilities capabilities = new DesiredCapabilities();
+  {
+      capabilities.setCapability("username", System.getProperty("SAUCE_USER"));
+      capabilities.setCapability("accessKey", System.getProperty("SAUCE_ACCESS_KEY"));
+      capabilities.setCapability("browserName", "Chrome");
+      capabilities.setCapability("platform", "Windows 10");
+      capabilities.setCapability("version", "59.0");
+      capabilities.setCapability("name", "selenium-jupiter-example");
+  }
 
   private void assertGoogleSearch(WebDriver driver) {
     final long timeoutSeconds = 10;
@@ -47,11 +60,18 @@ public class GoogleSearchTest {
   }
 
   @Test
+  void testWithSaucelabs(RemoteWebDriver driver) {
+      assertGoogleSearch(driver);
+  }
+
+  @Test
+  @Disabled("Kept as an example that runs with local Selenium Grid")
   public void testRemoteChrome(@DriverCapabilities("browserName=chrome") RemoteWebDriver driver) {
     assertGoogleSearch(driver);
   }
 
   @Test
+  @Disabled("Kept as an example that runs with local Selenium Grid")
   public void testRemoteFirefox(@DriverCapabilities("browserName=firefox") RemoteWebDriver driver) {
     assertGoogleSearch(driver);
   }
